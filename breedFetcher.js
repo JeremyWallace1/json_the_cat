11392ca8d9eb439a7456f442e5730b4d00ed0e48
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-24 14:05:59 -0700

    created index.js; moved process.argv code into it; refactored code and exporting fetchBreedDescription for use in index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,14 @@
 const request = require('request');
 
 const URL = 'https://api.thecatapi.com/v1/breeds/search?q=';
-const catBreed = process.argv.slice(2);
 
-request(URL + catBreed, (error, response, body) => {
-  if (error) {
-    console.log(error.message);
-    process.exit();
-  }
 
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("No information found for breed:", catBreed[0]);
-    process.exit();
-  }
-  console.log(data[0].description);
-  process.exit();
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(URL + breedName, (error, response, body) => {
+    const data = JSON.parse(body);
+    return data;
+  });
+  
+};
+
+const module.exports = { fetchBreedDescription };
