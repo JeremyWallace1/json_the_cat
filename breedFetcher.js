b7cd295e5c02afdb0e4280fff34ced24da236193
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-24 11:01:55 -0700

    made simple url fetch and query with argv to take an argument from the command line and edge cases for error and no information found

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,19 @@
 const request = require('request');
 
+const URL = 'https://api.thecatapi.com/v1/breeds/search?q=';
+const catBreed = process.argv.slice(2);
+
+request(URL + catBreed, (error, response, body) => {
+  if (error) {
+    console.log(error.message);
+    process.exit();
+  }
+
+  const data = JSON.parse(body);
+  if (data.length === 0) {
+    console.log("No information found for breed:", catBreed[0]);
+    process.exit();
+  }
+  console.log(data[0].description);
+  process.exit();
+});
